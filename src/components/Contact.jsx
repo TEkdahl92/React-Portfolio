9fc029f523cf61a2a19d2848a493e385a8de64c1
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-09-09 16:56:56 -0500

    chamges some colors to teal to lessen the strain on the eyes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/1ce408f0-54b2-4626-9733-e52028cf31b0" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-red-600 text-gray-300'>Contact</p>
+                <p className='text-4xl font-bold inline border-b-4 border-teal-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - troypekdahl@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
