f603201d79184c411a1934678be9a189c51a786d
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-09-19 14:33:11 -0500

    Updated my about section with a more in depth description of me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,9 +17,11 @@ const About = () => {
               <p>Hi. I'm Troy, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>I am passionate about making really creative experiences for the people around me.
-                I'm attempting to specialize in React due to the amazing performance enhancements it has for applications and the high
-                testability. I was born and raised in central Texas and I cant wait to meet you!  </p>  
+              <p>Full Stack web developer using my background in customer service to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the
+University of Texas at Austin, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as a fun and innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I applied my knowledge of 
+the MERN stack development in a recent project. I worked on a team of five to develop a multi-page MERN app that provides users with a more fun and enjoyable experience while searching for their favorite books or if they want to join a book club. I’m excited to
+leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the
+web.  </p>  
             </div>
           </div>
       </div>
