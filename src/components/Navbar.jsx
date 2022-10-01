354b7b98003b155c15fc76c123ab24a732c50e70
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-10-01 00:04:51 -0500

    added more detailed resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,8 +125,8 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='TroysResume.pdf' 
-              download='TroysResume.pdf'> 
+              href='troyEkdahlResume.pdf' 
+              download='troyEkdahlResume.pdf'> 
              Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
